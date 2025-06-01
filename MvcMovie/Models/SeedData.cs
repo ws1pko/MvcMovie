0268749bab6e1734e83b11f95c596372318bf30b
ws1pko <alexanderbin2321@gmail.com>
2025-06-01 22:16:28 +0400

    commit 6

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -22,16 +22,18 @@ public static class SeedData
             context.Movie.AddRange(
                 new Movie
                 {
-                    Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
-                    Price = 7.99M
+                   Title = "When Harry Met Sally",
+                   ReleaseDate = DateTime.Parse("1989-1-11"),
+                   Genre = "Romantic Comedy",
+                   Rating = "7",
+                   Price = 7.99M
                 },
                 new Movie
                 {
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "9",
                     Price = 8.99M
                 },
                 new Movie
@@ -39,6 +41,7 @@ public static class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "10",
                     Price = 9.99M
                 },
                 new Movie
@@ -46,6 +49,7 @@ public static class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "10",
                     Price = 3.99M
                 }
             );
